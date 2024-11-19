d9d0981765e7e2a7044e73a65a7b5c960be089be
Husam-AbuZina <husamzinap@gmail.com>
2024-11-19 16:32:51 +0200

    add layout box structure

--- src/components/box/box.jsx
+++ src/components/box/box.jsx
@@ -4,7 +4,17 @@ import './Box.css';
 const Box = () => {
   return (
     <div className="box">
-        <h1>I am a box</h1>
+      <div className='btns'>
+        <button>Pomodoro</button>
+        <button>Short Break</button>
+        <button>Long Break</button>
+      </div>
+      <div className='timer'>
+        <h1>25:00</h1>
+      </div>
+      <div className='start-btn'>
+        <button><h1>Start</h1></button>
+      </div>
     </div>
   );
 }
