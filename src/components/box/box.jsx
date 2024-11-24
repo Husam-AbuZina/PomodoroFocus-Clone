a181bcb4dbbd808d76cb0ef06b25b9af8cb65608
Husam-AbuZina <husamzinap@gmail.com>
2024-11-24 22:17:31 +0200

    remove comments

--- src/components/box/box.jsx
+++ src/components/box/box.jsx
@@ -11,9 +11,9 @@ const Box = ({ changeBackground }) => {
   }
 
   const modes = {
-    pomodoro: { time: 25 * 60, color: "#ff6347" }, // Tomato color for Pomodoro
-    shortBreak: { time: 5 * 60, color: "#87ceeb" }, // Sky blue for Short Break
-    longBreak: { time: 15 * 60, color: "#6a5acd" }, // Slate blue for Long Break
+    pomodoro: { time: 25 * 60, color: "#ff6347" },
+    shortBreak: { time: 5 * 60, color: "#87ceeb" },
+    longBreak: { time: 15 * 60, color: "#6a5acd" },
   };
 
 
